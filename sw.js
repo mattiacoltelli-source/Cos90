67128e938064604e7adb069b75af9840e82c2b60
mattiacoltelli-source <mattia.coltelli@gmail.com>
2026-03-18 00:55:55 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,14 +1,7 @@
-// Service Worker minimale per evitare cache vecchie
-
-self.addEventListener("install", (event) => {
-  // Attiva subito il nuovo service worker
+self.addEventListener('install', e => {
   self.skipWaiting();
 });
 
-self.addEventListener("activate", (event) => {
-  // Prende il controllo immediatamente
-  event.waitUntil(self.clients.claim());
+self.addEventListener('activate', e => {
+  self.clients.claim();
 });
-
-// NON intercettiamo le fetch, quindi nessuna cache
-self.addEventListener("fetch", () => {});
